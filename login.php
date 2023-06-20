8ce3c8d9e59704fa675a889b2e6bfae9a25b0ba1
ziaddahmeddd <96743534+ziaddahmeddd@users.noreply.github.com>
2023-06-20 05:49:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-    session_start(); // start session at the beginning
+    session_start(); 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
